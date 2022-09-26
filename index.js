0b0237bd92ccb44fc8fa6e4f4e71422c77b641d6
Bugra Sen <bugra.sen@bil.omu.edu.tr>
2022-09-26 21:42:25 +0300

    axios-

---
+++ index.js
@@ -0,0 +1,6 @@
+import getData from "./app.js" // getData'yı import ettik. default olarak export ettiğimiz için getData yerine herhangi bir isimlendirmede koyabiliriz.
+import axios from "axios";
+
+getData(1)  // getData(userId) olan fonk. 1 değerini vererek çalıştırdık.
+    .then(data => console.log(data)) // çıktılar.
+    .catch(e => console.log(e)) // hata olursa verilecek çıktı
